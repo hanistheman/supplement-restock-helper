0bdd50b8690d7f0aa3899ff4599ebfe5af00528b
hanistheman <hangao03@gmail.com>
2026-08-01 14:50:05 -0500

    Setup database connection module in project

---
+++ backend/src/database.py
@@ -0,0 +1,27 @@
+""" Database connection setup. 
+
+This is where we handle SQLite. 
+Should we switch to another database, like Postgres, 
+we change this. All other stuff talks to SQLAlchemy.  
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker, DeclarativeBase
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./supplements.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    """All ORM models inherit from here."""
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
