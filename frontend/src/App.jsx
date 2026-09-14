84c7b100165c77594a66aeb1d2deb850620e3c63
hanistheman <hangao03@gmail.com>
2026-09-13 21:21:38 -0500

    Update app.jsx to include new modules created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 import NavBar from "./components/NavBar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import ShelfPage from "./pages/ShelfPage";
 import AboutPage from "./pages/AboutPage";
 import HelpPage from "./pages/HelpPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <NavBar />
       <Routes>
-        <Route path="/" element={<ShelfPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <ShelfPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/help" element={<HelpPage />} />
       </Routes>
-    </>
+    </AuthProvider>
   );
 }
